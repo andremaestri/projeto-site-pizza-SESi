a290302d6d00a1990cc4bd953c12a51bd8e741cb
andremaestri <andre_m_bueno@estudante.sesisenai.org.br>
2026-05-04 12:15:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const toggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 const overlay = document.getElementById("overlay");
 
+
 toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    overlay.classList.toggle("active");
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
+
 overlay.addEventListener("click", () => {
-    menu.classList.remove("active");
-    overlay.classList.remove("active");
-})
+  menu.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
